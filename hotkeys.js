abce58a30577a5ffb8ac2d62de43045b4a1023f9
linkiret <57070044+linkiret@users.noreply.github.com>
2024-03-15 00:36:48 +0700

    Add files via upload

---
+++ hotkeys.js
@@ -0,0 +1,72 @@
+Lampa.Platform.tv();
+function log() {
+        console.log.apply(console.log, arguments);
+      }
+log('Hotkeys', 'Hotkeys 171 loaded');
+
+function openPanel(element) {
+	if ('parseFloat(Lampa.Manifest.app_version)' >= '1.7') {
+        //log('Hotkeys', '1.7.0');
+		Lampa.Utils.trigger(document.querySelector(element), 'click');
+	} else {
+        //log('Hotkeys', 'old version');
+		document.querySelector(element).click();
+	}
+};
+
+function listenDestroy() {
+	document.removeEventListener("keydown", listenHotkeys);
+	Lampa.Player.listener.remove('destroy', listenDestroy);	
+};
+
+function startHotkeys() {
+	document.addEventListener("keydown", listenHotkeys);
+	Lampa.Player.listener.follow('destroy', listenDestroy);
+};
+
+function listenHotkeys(e) {
+
+//log('Hotkeys', e.keyCode);
+	if (e.keyCode === e.keyCode) {
+  alert(e.keyCode);
+  }
+  if (e.keyCode === 427) {
+	openPanel('.player-panel__next.button.selector');
+  }
+
+  if (e.keyCode === 167 || e.keyCode === 428 || e.keyCode === 28 || e.keyCode === 34) {
+	openPanel('.player-panel__prev.button.selector');
+  }
+	
+  if (e.keyCode === 48) {
+    //log('Hotkeys', '0 pressed');
+    if (!document.querySelector('body.selectbox--open')) {
+	//log('Hotkeys', 'subs list not visible');
+	openPanel('.player-panel__subs.button.selector');
+    } else {
+      	history.back();
+    }
+  }
+  if (e.keyCode === 53) {
+    //log('Hotkeys', '5 pressed');
+    if (!document.querySelector('body.selectbox--open')) {
+	//log('Hotkeys', 'playlist not visible');
+      	openPanel('.player-panel__playlist.button.selector');
+        alert('пидор работает');
+    } else {
+      	history.back();
+        alert('пидор не работает');
+    }
+  }
+  if (e.keyCode === 56) {
+    //log('Hotkeys', '8 pressed');
+    if (!document.querySelector('body.selectbox--open')) {
+	//log('Hotkeys', 'audio list not visible');
+      	openPanel('.player-panel__tracks.button.selector');
+    } else {
+      	history.back();
+    }
+  }
+};
+
+Lampa.Player.listener.follow('ready',startHotkeys);
