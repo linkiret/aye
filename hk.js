808632c9a6bab0088e7e0b929907e2ad48a07881
linkiret <57070044+linkiret@users.noreply.github.com>
2024-04-11 06:42:13 +0700

    Update hk.js

--- hk.js
+++ hk.js
@@ -17,7 +17,7 @@ function startHotkeys() {
 
 function listenHotkeys(e) {
 
-	if (e.keyCode === 37 || 38 || 39 || 40) {
+	if (document.querySelector(".player--panel-visible")) {
 		aye = setInterval(foca, 2500);
 	}
   if (e.keyCode === 406) {
