18fd8eb6a2292295e1094cb6938050267e7b1bef
linkiret <57070044+linkiret@users.noreply.github.com>
2024-04-11 04:09:13 +0700

    Update hk.js

--- hk.js
+++ hk.js
@@ -1,16 +1,4 @@
 Lampa.Platform.tv();
-function log() {
-        console.log.apply(console.log, arguments);
-      }
-log('Hotkeys', 'Hotkeys loaded');
-
-function openPanel(element) {
-	if ('parseFloat(Lampa.Manifest.app_version)' >= '1.7') {
-		Lampa.Utils.trigger(document.querySelector(element), 'click');
-	} else {
-		document.querySelector(element).click();
-	}
-};
 
 function listenDestroy() {
 	document.removeEventListener("keydown", listenHotkeys);
@@ -28,22 +16,13 @@ function listenHotkeys(e) {
   Lampa.PlayerVideo.rewind(true,80);
   }
   if (e.keyCode === 427) {
-  	Lampa.Noty.show(e.keyCode);
-	openPanel('.player-panel__next.button.selector');
+	Lampa.PlayerPlaylist.next();
   }
 
   if (e.keyCode === 428) {
-  	Lampa.Noty.show(e.keyCode);
-	openPanel('.player-panel__prev.button.selector');
+	Lampa.PlayerPlaylist.prev();
   }
-	
-  if (e.keyCode === 405) {
-    if (!document.querySelector('body.selectbox--open')) {
-      	openPanel('.player-panel__playlist.button.selector');
-    } else {
-      	history.back();
-    }
-  }
+
 };
 
 Lampa.Player.listener.follow('ready',startHotkeys);
